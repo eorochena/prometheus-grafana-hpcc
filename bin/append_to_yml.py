665124276dcd1a2a45aaecdbffa124cb52e80276
Eduardo Orochena <mint.alchemist@gmail.com>
2017-06-15 14:10:19 -0400

    s

--- bin/append_to_yml.py
+++ bin/append_to_yml.py
@@ -8,6 +8,20 @@ roxie_cluster = server_list.Roxie()
 thor_cluster = server_list.ThorSlave()
 
 if thor_cluster:
+    # dealing with multiple clusters sharing same hardware
+    unique_values = []
+    keys_with_duplicate_values = []
+    for key, value in thor_cluster.items():
+        #print('k ', key)
+        #print('v ', value)
+        if value not in unique_values:
+            unique_values.append(value)
+        elif value in unique_values:
+            keys_with_duplicate_values.append(key)
+    print('FINAL    ', keys_with_duplicate_values)
+    for deletion in keys_with_duplicate_values:
+        del thor_cluster[deletion]
+    print(thor_cluster)
     for cluster_name in thor_cluster:
         targets = []
         for server in thor_cluster[cluster_name]:
