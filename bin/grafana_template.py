45454596fbc66e626751154c5a5a7463220f7399
Eduardo Orochena <mint.alchemist@gmail.com>
2017-06-17 23:52:53 -0400

    minor change

--- bin/grafana_template.py
+++ bin/grafana_template.py
@@ -7,10 +7,11 @@ name = gethpcc.environment_name()
 json_file = name + '.json'
 file_location = os.path.abspath("../json_files/" + json_file)
 new_json =open(file_location, 'w+')
+template_name = 'cluster-template'
 
 with open('../grafana-template.json', 'r+') as template:
     for line in template:
-        if 'cluster-template' in line:
+        if line.find('cluster-template') >= 0:
             new_line = line.replace('cluster-template', name).replace('\n', '')
             new_json.write(new_line)
         else:
